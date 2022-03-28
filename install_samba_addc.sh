7f9196f60c06ac6c5a0dbbd5a8bfda5d16c8fb0f
wesscd <wesscd@gmail.com>
2022-03-28 16:27:31 -0300

    Update install_samba_addc.sh

--- install_samba_addc.sh
+++ install_samba_addc.sh
@@ -187,6 +187,8 @@ network:
     echo "----------------------------------------"
     echo "----------------------------------------"
     
+    sleep 10
+    
     apt-get install wget acl attr autoconf bind9utils bison build-essential debhelper dnsutils docbook-xml docbook-xsl flex gdb libjansson-dev krb5-user libacl1-dev libaio-dev libarchive-dev libattr1-dev libblkid-dev libbsd-dev libcap-dev libcups2-dev libgnutls28-dev libgpgme-dev libjson-perl libldap2-dev libncurses5-dev libpam0g-dev libparse-yapp-perl libpopt-dev libreadline-dev nettle-dev perl pkg-config python-all-dev python2-dbg python-dev-is-python2 python3-dnspython python3-gpg python3-markdown python3-dev xsltproc zlib1g-dev liblmdb-dev lmdb-utils libsystemd-dev perl-modules-5.30 libdbus-1-dev libtasn1-bin -y 
     
     sleep 2
@@ -375,13 +377,19 @@ network:
 
     systemctl start samba-ad-dc.service
 
-    echo
-        "
+    echo "
         +------------------------------------+
         |       SAMBA SERVICE INSTALLED      |    
         +------------------------------------+
-        Ex.: FILES
+        
+	
+	FQND - $FQDN -> To connect on Active Directory.
+	
+	HOSTNAME - $HOSTNAME 
+	
+	NETBIOS - $NETBIOS -> To configure chgrp file server.
+	
+	
+	
         "
     echo
-
-    clear
